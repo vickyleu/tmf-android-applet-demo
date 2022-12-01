e253d8d96eee3873c94751157b3da55ab39a411c
robincxiao <516282426@qq.com>
2022-12-01 11:54:19 +0800

    更新sdk添加wx.login和wx.getUserInfo等事件监听

---
+++ module-applet/src/main/java/com/tencent/tmf/applet/demo/jsplugin/WxApiPlugin.java
@@ -0,0 +1,53 @@
+package com.tencent.tmf.applet.demo.jsplugin;
+
+import com.tencent.qqmini.sdk.annotation.JsEvent;
+import com.tencent.qqmini.sdk.annotation.JsPlugin;
+import com.tencent.qqmini.sdk.launcher.core.model.RequestEvent;
+import com.tencent.qqmini.sdk.launcher.core.plugins.BaseJsPlugin;
+
+import org.json.JSONObject;
+
+@JsPlugin(secondary = true)
+public class WxApiPlugin extends BaseJsPlugin {
+    /**
+     * 对应小程序wx.login调用
+     * @param req
+     */
+    @JsEvent("wx.login")
+    public void login(final RequestEvent req) {
+        //获取参数
+        //req.jsonParams
+        //异步返回数据
+        //req.fail();
+        //req.ok();
+        req.ok(new JSONObject());
+    }
+
+    /**
+     * 对应小程序wx.getUserInfo调用
+     * @param req
+     */
+    @JsEvent("wx.getUserInfo")
+    public void getUserInfo(final RequestEvent req) {
+        //获取参数
+        //req.jsonParams
+        //异步返回数据
+        //req.fail();
+        //req.ok();
+        req.ok(new JSONObject());
+    }
+
+    /**
+     * 对应小程序wx.getUserProfile调用
+     * @param req
+     */
+    @JsEvent("wx.getUserProfile")
+    public void getUserProfile(final RequestEvent req) {
+        //获取参数
+        //req.jsonParams
+        //异步返回数据
+        //req.fail();
+        //req.ok();
+        req.ok(new JSONObject());
+    }
+}
